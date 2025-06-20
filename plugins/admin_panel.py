b2c274235d4ef2fcc6c3d77512e5bc60d2cfde4b
sainallamilli <142711611+sainallamilli@users.noreply.github.com>
2025-06-20 16:47:16 +0530

    Update admin_panel.py

--- plugins/admin_panel.py
+++ plugins/admin_panel.py
@@ -17,17 +17,8 @@ is_restarting = False
 
 @Client.on_message(filters.private & filters.command("restart") & filters.user(ADMIN_USER_ID))
 async def restart_bot(b, m):
-    global is_restarting
-    if not is_restarting:
-        is_restarting = True
-        await m.reply_text("**Restarting.....**")
-
-        # Gracefully stop the bot's event loop
-        b.stop()
-        time.sleep(2)  # Adjust the delay duration based on your bot's shutdown time
-
-        # Restart the bot process
-        os.execl(sys.executable, sys.executable, *sys.argv)
+    await m.reply_text("Restarting...")
+    os.execl(sys.executable, sys.executable, "-m", "bot")
 
 @Client.on_message(filters.private & filters.command("leaderboard"))
 #@Client.on_message(filters.command("leaderboard") & filters.user(Config.ADMIN))
